50133f35ad8cfe34909e9f32593c1465ee1df5ba
Erin Beasley <ebeasley@andrew.cmu.edu>
2019-01-20 19:59:47 -0500

    yep

--- cprogramminglab-handout/queue.h
+++ cprogramminglab-handout/queue.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * Code for basic C skills diagnostic.
  * Developed for courses 15-213/18-213/15-513 by R. E. Bryant, 2017
  * Extended to store strings, 2018
@@ -26,6 +26,8 @@ typedef struct ELE {
 /* Queue structure */
 typedef struct {
     list_ele_t *head;  /* Linked list of elements */
+    list_ele_t *tail;
+    int size;
     /*
       You will need to add more fields to this structure
       to efficiently implement q_size and q_insert_tail
@@ -46,45 +48,3 @@ queue_t *q_new();
 */
 void q_free(queue_t *q);
 
-/*
-  Attempt to insert element at head of queue.
-  Return true if successful.
-  Return false if q is NULL or could not allocate space.
-  Argument s points to the string to be stored.
-  The function must explicitly allocate space and copy the string into it.
- */
-bool q_insert_head(queue_t *q, char *s);
-
-/*
-  Attempt to insert element at tail of queue.
-  Return true if successful.
-  Return false if q is NULL or could not allocate space.
-  Argument s points to the string to be stored.
-  The function must explicitly allocate space and copy the string into it.
- */
-bool q_insert_tail(queue_t *q, char *s);
-
-/*
-  Attempt to remove element from head of queue.
-  Return true if successful.
-  Return false if queue is NULL or empty.
-  If sp is non-NULL and an element is removed, copy the removed string to *sp
-  (up to a maximum of bufsize-1 characters, plus a null terminator.)
-  The space used by the list element and the string should be freed.
-*/
-bool q_remove_head(queue_t *q, char *sp, size_t bufsize);
-
-/*
-  Return number of elements in queue.
-  Return 0 if q is NULL or empty
- */
-int q_size(queue_t *q);
-
-/*
-  Reverse elements in queue
-  No effect if q is NULL or empty
-  This function should not allocate or free any list elements
-  (e.g., by calling q_insert_head, q_insert_tail, or q_remove_head).
-  It should rearrange the existing ones.
- */
-void q_reverse(queue_t *q);
